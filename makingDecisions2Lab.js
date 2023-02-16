70a63ead2e98e78a8eb93e87bcad666db4978666
Seth Reid <reid.seth.96@gmail.com>
2023-02-16 15:08:32 -0700

    Completed all problems

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -205,6 +205,12 @@ console.log(text)
 //Code Here
 for (i = 1; i < 101; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    
+    console.log("Devmountain")
+  } else if (i % 3 === 0) {
+    console.log("Dev")
+  } else if (i % 5 === 0) {
+    console.log("mountain")
+  } else {
+    console.log(i)
   }
 }
